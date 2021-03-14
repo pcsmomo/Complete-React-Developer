9b4860500350ad3945dae2b8cd93f837ab0602a8
dwnoahkim <dw.noah.kim@gmail.com>
2021-03-14 23:54:33 +1100

    26. Dynamic Content

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,30 +6,28 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'Hellow Noah',
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'id1',
+        },
+        {
+          name: 'Dracula',
+          id: 'id2',
+        },
+        {
+          name: 'Zombie',
+          id: 'id3',
+        },
+      ],
     };
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React with Noah
-          </a>
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: 'Hello Mei' })}>
-            Change Text
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return <h1 key={monster.id}>{monster.name}</h1>;
+        })}
       </div>
     );
   }
