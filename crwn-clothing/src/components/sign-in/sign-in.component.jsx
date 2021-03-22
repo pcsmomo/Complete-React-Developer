39a6a95853cc557c0536a2057adc843a5345cb59
pcsmomo <dongwoo_333@hanmail.net>
2021-03-22 22:33:49 +1100

    86. Form Input Component

--- crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component';
+
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -31,22 +33,24 @@ class SignIn extends React.Component {
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
-          <input
+          <FormInput
             name="email"
             type="email"
             onChange={this.handleChange}
             value={this.state.email}
+            label="email"
             requried
           />
           <label>Email</label>
-          <input
+          <FormInput
             name="password"
             type="password"
             value={this.state.password}
             onChange={this.handleChange}
+            label="password"
             requried
           />
-          <label>Email</label>
+          <label>Password</label>
 
           <input type="submit" value="Submit Form" />
         </form>
