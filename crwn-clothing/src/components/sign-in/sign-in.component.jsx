f0357014797b8ee62cb67234157ab283a61d0c54
pcsmomo <dongwoo_333@hanmail.net>
2021-03-29 18:08:42 +1100

    93. Google Sign In Authentication

--- crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
-import CustomBottn from '../custom-button/custom-button.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
-import CustomButton from '../custom-button/custom-button.component';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -52,6 +53,10 @@ class SignIn extends React.Component {
             requried
           />
           <CustomButton type="submit"> Sign in </CustomButton>
+          <CustomButton onClick={signInWithGoogle}>
+            {' '}
+            Sign in with Google{' '}
+          </CustomButton>
         </form>
       </div>
     );
