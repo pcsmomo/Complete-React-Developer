0e49ca3be662d119043dce9ec39e6ba4598b0897
pcsmomo <dongwoo_333@hanmail.net>
2021-04-12 19:43:32 +1000

    134. Checkout Page 2

--- crwn-clothing/src/redux/cart/cart.selectors.js
+++ crwn-clothing/src/redux/cart/cart.selectors.js
@@ -16,8 +16,16 @@ export const selectCartItemCount = createSelector(
   [selectCartItems],
   (cartItems) =>
     cartItems.reduce(
-      (accumalatedQuantity, cartItem) =>
-        accumalatedQuantity + cartItem.quantity,
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
       0
     )
+);
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce(
+    (accumulatedTotal, cartItem) =>
+      accumulatedTotal + cartItem.quantity * cartItem.price,
+    0
+  )
 );
