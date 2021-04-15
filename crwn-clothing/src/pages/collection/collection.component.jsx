d035b3fc48220daa6d7d7fbee950ca28615147ee
pcsmomo <dongwoo_333@hanmail.net>
2021-04-16 07:49:19 +1000

    146. Improving Naming Of Component

---
+++ crwn-clothing/src/pages/collection/collection.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import "./collection.styles.scss";
+
+const CollectionPage = (props) => {
+  console.log(props.match.params.collectionId);
+  return (
+    <div className="collection-page">
+      <h2>COLLECTION PAGE</h2>
+    </div>
+  );
+};
+
+export default CollectionPage;
