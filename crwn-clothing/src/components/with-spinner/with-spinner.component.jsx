6c2ec435996a7e1eb4f042aa67ad662a8ea36026
pcsmomo <dongwoo_333@hanmail.net>
2021-04-22 16:15:52 +1000

    reduce code

--- crwn-clothing/src/components/with-spinner/with-spinner.component.jsx
+++ crwn-clothing/src/components/with-spinner/with-spinner.component.jsx
@@ -2,17 +2,14 @@ import React from "react";
 
 import { SpinnerOverlay, SpinnerContainer } from "./with-spinner.styles";
 
-const WithSpinner = (WrappedComponent) => {
-  const Spinner = ({ isLoading, ...otherProps }) => {
-    return isLoading ? (
-      <SpinnerOverlay>
-        <SpinnerContainer />
-      </SpinnerOverlay>
-    ) : (
-      <WrappedComponent {...otherProps} />
-    );
-  };
-  return Spinner;
+const WithSpinner = (WrappedComponent) => ({ isLoading, ...otherProps }) => {
+  return isLoading ? (
+    <SpinnerOverlay>
+      <SpinnerContainer />
+    </SpinnerOverlay>
+  ) : (
+    <WrappedComponent {...otherProps} />
+  );
 };
 
 export default WithSpinner;
