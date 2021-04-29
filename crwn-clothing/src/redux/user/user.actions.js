334ac69f6a40cb9eb8ac9a164f5d3767c729c819
pcsmomo <dongwoo_333@hanmail.net>
2021-04-30 03:36:51 +1000

    206. Sign Out With Sagas

--- crwn-clothing/src/redux/user/user.actions.js
+++ crwn-clothing/src/redux/user/user.actions.js
@@ -22,3 +22,16 @@ export const signInFailure = (error) => ({
 export const checkUserSession = () => ({
   type: UserActionTypes.CHECK_USER_SESSION,
 });
+
+export const signOutStart = () => ({
+  type: UserActionTypes.SIGN_OUT_START,
+});
+
+export const signOutSuccess = (user) => ({
+  type: UserActionTypes.SIGN_OUT_SUCCESS,
+});
+
+export const signOutFailure = (error) => ({
+  type: UserActionTypes.SIGN_OUT_FAILURE,
+  payload: error,
+});
