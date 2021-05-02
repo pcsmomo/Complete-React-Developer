e598cede6297fea99f0cd2d3cebc7529c834c05d
pcsmomo <dongwoo_333@hanmail.net>
2021-05-02 15:16:13 +1000

    212. Hook Rules

--- use-effect-example/src/components/use-effect-example/use-effect-example.component.jsx
+++ use-effect-example/src/components/use-effect-example/use-effect-example.component.jsx
@@ -1,29 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Card from '../card/card.component';
+import Card from "../card/card.component";
 
 const UseEffectExample = () => {
   const [user, setUser] = useState(null);
-  const [searchQuery, setSearchQuery] = useState('Bret');
+  const [searchQuery, setSearchQuery] = useState("Bret");
 
   useEffect(() => {
-    const fetchFunc = async () => {
-      const response = await fetch(
-        `https://jsonplaceholder.typicode.com/users?username=${searchQuery}`
-      );
-      const resJson = await response.json();
-      setUser(resJson[0]);
-    };
+    if (searchQuery.length > 0) {
+      const fetchFunc = async () => {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/users?username=${searchQuery}`
+        );
+        const resJson = await response.json();
+        setUser(resJson[0]);
+      };
 
-    fetchFunc();
+      fetchFunc();
+    }
   }, [searchQuery]);
 
   return (
     <Card>
       <input
-        type='search'
+        type="search"
         value={searchQuery}
-        onChange={event => setSearchQuery(event.target.value)}
+        onChange={(event) => setSearchQuery(event.target.value)}
       />
       {user ? (
         <div>
