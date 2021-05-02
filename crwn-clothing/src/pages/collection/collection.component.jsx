76832f2edd709d58faf087999afbfc32986c928f
pcsmomo <dongwoo_333@hanmail.net>
2021-05-02 15:54:07 +1000

    215. useEffect as ComponentWillUnmount()

--- crwn-clothing/src/pages/collection/collection.component.jsx
+++ crwn-clothing/src/pages/collection/collection.component.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 
 import CollectionItem from "../../components/collection-item/collection-item.component";
 
 import { selectCollection } from "../../redux/shop/shop.selector";
 
+import { firestore } from "../../firebase/firebase.utils";
+
 import "./collection.styles.scss";
 
 const CollectionPage = ({ collection }) => {
+  // useEffect(() => {
+  //   console.log("I am subscribing");
+  //   const unsubscribeFromCollections = firestore
+  //     .collection("collections")
+  //     .onSnapshot(async (snapshot) => console.log(snapshot));
+
+  //   return () => {
+  //     console.log("I am unsubscribing");
+  //     unsubscribeFromCollections();
+  //   };
+  // }, []);
+
   const { title, items } = collection;
 
   return (
