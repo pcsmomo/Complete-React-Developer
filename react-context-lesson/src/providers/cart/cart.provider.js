452118a040826781ac70f09eb28758702d1b44ee
pcsmomo <dongwoo_333@hanmail.net>
2021-05-12 20:02:04 +1000

    241. Provider Context Pattern 2 COMPLETE

--- react-context-lesson/src/providers/cart/cart.provider.js
+++ react-context-lesson/src/providers/cart/cart.provider.js
@@ -5,6 +5,7 @@ import {
   removeItemFromCart,
   filterItemFromCart,
   getCartItemsCount,
+  getCartTotal,
 } from "./cart.utils";
 
 export const CartContext = createContext({
@@ -15,12 +16,14 @@ export const CartContext = createContext({
   removeItem: () => {},
   clearItemFromCart: () => {},
   cartItemsCount: 0,
+  cartTotal: 0,
 });
 
 const CartProvider = ({ children }) => {
   const [hidden, setHidden] = useState(true);
   const [cartItems, setCartItems] = useState([]);
   const [cartItemsCount, setCartItemsCount] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
 
   const addItem = (item) => setCartItems(addItemToCart(cartItems, item));
   const removeItem = (item) =>
@@ -31,6 +34,7 @@ const CartProvider = ({ children }) => {
 
   useEffect(() => {
     setCartItemsCount(getCartItemsCount(cartItems));
+    setCartTotal(getCartTotal(cartItems));
   }, [cartItems]);
 
   return (
@@ -43,6 +47,7 @@ const CartProvider = ({ children }) => {
         removeItem,
         cartItemsCount,
         clearItemFromCart,
+        cartTotal,
       }}
     >
       {children}
