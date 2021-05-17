5f591b160aa06fc4698696d6c57b0fddd76ebc7f
pcsmomo <dongwoo_333@hanmail.net>
2021-05-17 10:05:57 +1000

    260. CartItem Count With Apollo

--- crwn-graphql-lesson/src/components/cart-icon/cart-icon.container.jsx
+++ crwn-graphql-lesson/src/components/cart-icon/cart-icon.container.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Mutation, Query } from "react-apollo";
+import { graphql } from "react-apollo";
 import { gql } from "apollo-boost";
+import { flowRight } from "lodash";
 
 import CartIcon from "./cart-icon.component";
 
@@ -16,16 +17,42 @@ const GET_ITEM_COUNT = gql`
   }
 `;
 
-const CartIconContainer = () => (
-  <Query query={GET_ITEM_COUNT}>
-    {({ data: { itemCount } }) => (
-      <Mutation mutation={TOGGLE_CART_HIDDEN}>
-        {(toggleCartHidden) => (
-          <CartIcon toggleCartHidden={toggleCartHidden} itemCount={itemCount} />
-        )}
-      </Mutation>
-    )}
-  </Query>
-);
+const CartIconContainer = ({ data: { itemCount }, mutate }) => {
+  const toggleCartHidden = mutate;
+  return <CartIcon toggleCartHidden={toggleCartHidden} itemCount={itemCount} />;
+};
 
-export default CartIconContainer;
+export default flowRight(
+  graphql(GET_ITEM_COUNT),
+  graphql(TOGGLE_CART_HIDDEN)
+)(CartIconContainer);
+
+/** Extra query and mutation for a test */
+/** Cannot receive two queries nor mutations.
+ * The second one overwrites the first one.
+ * Query comes to "data" and Mutation comes to "mutate".
+ */
+
+// const GET_CART_HIDDEN = gql`
+//   {
+//     cartHidden @client
+//   }
+// `;
+
+// const ADD_ITEM_TO_CART = gql`
+//   mutation AddItemToCart($item: Item!) {
+//     addItemToCart(item: $item) @client
+//   }
+// `;
+
+// const CartIconContainer = (props) => {
+//   console.log(props);
+//   return <CartIcon />;
+// };
+
+// export default flowRight(
+//   graphql(GET_ITEM_COUNT),
+//   graphql(GET_CART_HIDDEN),
+//   graphql(TOGGLE_CART_HIDDEN),
+//   graphql(ADD_ITEM_TO_CART)
+// )(CartIconContainer);
