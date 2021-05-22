92ec50ca5a2fc2c2505ea7d1ca77026f19817255
pcsmomo <dongwoo_333@hanmail.net>
2021-05-23 07:29:43 +1000

    273. useCallback

--- useMemo-useCallback/src/App.js
+++ useMemo-useCallback/src/App.js
@@ -1,6 +1,8 @@
-import React, { useState, useCallback, useMemo } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const functions = new Set();
 
 const App = () => {
   const [count1, setCount1] = useState(0);
@@ -8,21 +10,22 @@ const App = () => {
 
   const incrementCount1 = useCallback(() => setCount1(count1 + 1), [count1]);
   const incrementCount2 = useCallback(() => setCount2(count2 + 1), [count2]);
+  const logName = useCallback(() => console.log("Noah"), []);
 
-  const doSomethingComplicated = useMemo(() => {
-    console.log('I am computing something complex');
-    return ((count1 * 1000) % 12.4) * 51000 - 4000;
-  }, [count1]);
+  functions.add(logName);
+  functions.add(incrementCount1);
+  functions.add(incrementCount2);
+
+  console.log(functions);
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         Count1: {count1}
         <button onClick={incrementCount1}>Increase Count1</button>
         Count2: {count2}
         <button onClick={incrementCount2}>Increase Count2</button>
-        complexValue: {doSomethingComplicated}
       </header>
     </div>
   );
