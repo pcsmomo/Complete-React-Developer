d4b3e5a85578971cb3fafd100e97bd9bed51a893
pcsmomo <dongwoo_333@hanmail.net>
2021-06-08 21:26:52 +1000

    331. Testing Connected Components 2

--- testing_robofriends-pwa/src/components/MainPage.test.js
+++ testing_robofriends-pwa/src/components/MainPage.test.js
@@ -39,3 +39,21 @@ it("filters robots correctly", () => {
     },
   ]);
 });
+
+it("filters robots correctly 2", () => {
+  const mockProps3 = {
+    onRequestRobots: jest.fn(),
+    robots: [
+      {
+        id: 3,
+        name: "John",
+        email: "John@gmail.com",
+      },
+    ],
+    searchField: "a",
+    isPending: false,
+  };
+  const filteredRobots = [];
+  const wrapper3 = shallow(<MainPage {...mockProps3} />);
+  expect(wrapper3.instance().filterRobots([])).toEqual(filteredRobots);
+});
