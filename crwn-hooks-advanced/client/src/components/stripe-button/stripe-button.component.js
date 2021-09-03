6adf9a0567b5f3ba990670b896156260db09b05a
pcsmomo <dongwoo_333@hanmail.net>
2021-09-04 08:21:35 +1000

    366. React-Redux Hooks: START

---
+++ crwn-hooks-advanced/client/src/components/stripe-button/stripe-button.component.js
@@ -0,0 +1,47 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_qX5l3nkBerpHx6C08VrGriCo";
+
+  const onToken = (token) => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please make sure you use the provided credit card."
+        );
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+      locale="en"
+      currency="AUD"
+    />
+  );
+};
+
+export default StripeCheckoutButton;
