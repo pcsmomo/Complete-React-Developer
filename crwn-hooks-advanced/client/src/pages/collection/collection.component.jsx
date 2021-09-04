064d29d11ca36b2ffa2ac9a99ae8381634628922
pcsmomo <dongwoo_333@hanmail.net>
2021-09-04 13:17:44 +1000

    369. useParams Part 2

--- crwn-hooks-advanced/client/src/pages/collection/collection.component.jsx
+++ crwn-hooks-advanced/client/src/pages/collection/collection.component.jsx
@@ -1,13 +1,16 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
-import CollectionItem from "../../components/collection-item/collection-item.component";
+import CollectionItem from '../../components/collection-item/collection-item.component';
 
-import { selectCollection } from "../../redux/shop/shop.selector";
+import { selectCollection } from '../../redux/shop/shop.selector';
 
-import "./collection.styles.scss";
+import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => {
+const CollectionPage = () => {
+  const { collectionId } = useParams();
+  const collection = useSelector(selectCollection(collectionId));
   const { title, items } = collection;
 
   return (
@@ -22,8 +25,4 @@ const CollectionPage = ({ collection }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state),
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
